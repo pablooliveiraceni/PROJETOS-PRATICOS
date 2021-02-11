15e9105eb41075846fed820b8ea7b70c2b23d29e
pablooliveiraceni <cenipablo16@gmail.com>
2021-02-11 16:55:52 -0300

    Adicionando projeto ssr

---
+++ REACT-BACK/app6-ssr/src/App.js
@@ -0,0 +1,13 @@
+import Copyright from './Copyrigth';
+
+function App() {
+
+      return (
+
+            <Copyright ano="2021" />
+
+    );
+
+}
+
+export default App;
---
+++ REACT-BACK/app6-ssr/src/Copyrigth.jsx
@@ -0,0 +1,5 @@
+export default props => {
+
+    return <h1>Recode Pro {props.ano}!</h1>;
+
+};
